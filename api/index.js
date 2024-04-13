d0cfb08e6c0ab4f8e839ea50cf694521e56f939f
pooja gilda <pooja.maheshwari1312@gmail.com>
2024-04-13 19:16:47 +0530

    create middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -16,3 +16,12 @@ app.listen(8000,()=>{
 })
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
+app.use((err,req,res,next)=>{
+        const statusCode = err.statusCode || 500;
+        const message = err.message || 'internal server Error'
+        return res.status(statusCode).json({
+            success : false,
+            statusCode,
+            message
+        })
+})
