9eea8a0e39d5b93e383c28730f647ced950216cc
pooja gilda <pooja.maheshwari1312@gmail.com>
2024-04-13 12:35:48 +0530

    create user  model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import { Timestamp } from "bson";
+import mongoose  from "mongoose";
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required: true,
+        unique: true,   
+    },
+    email:{
+        type:String,
+        required: true,
+        unique: true,   
+    } ,
+    password:{
+        type:String,
+        required: true,
+        
+    },
+
+},{Timestamp:true})
+const User = mongoose.model('user',userSchema);
+export default User; 
