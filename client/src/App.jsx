79f8a13cd4149af9cc8c0188732fe80f2040a52e
pooja gilda <pooja.maheshwari1312@gmail.com>
2024-05-02 12:50:37 +0530

    update listing functionality added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,24 +5,25 @@ import Profile from "./pages/Profile";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
 import { Home } from "./pages/Home";
-import { Header}  from "./components/Header";
+import { Header } from "./components/Header";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { CreateList } from "./pages/CreateList";
+import { UpdateListing }from "./pages/UpdateListing";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/sign-in" element={<SignIn/>}></Route>
-        <Route path="/sign-up" element={<SignUp/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
-<Route element={<PrivateRoute/>}>
-<Route path="/profile" element={<Profile/>}></Route> 
-<Route path="/create-list" element={<CreateList/>}></Route>
-</Route>
-    
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/create-list" element={<CreateList />}></Route>
+          <Route path = "/update-listing/:listingId" element={<UpdateListing/>}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -278,7 +278,8 @@ console.log(error.message)
             </Link>
             <div className="flex flex-col item-center">
               <button  onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
-              <button className="text-green-700 uppercase">Edit</button>
+             <Link to={`/update-listing/${listing._id}`}>  <button className="text-green-700 uppercase">Edit</button></Link>
+              
             </div>
           </div>
         ))}
