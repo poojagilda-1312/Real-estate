dc61de7d18e20f626179b22b0f61c57244431681
pooja gilda <pooja.maheshwari1312@gmail.com>
2024-05-16 16:10:00 +0530

    changes in vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://real-estate-4rd4.onrender.com",
-        secure: false,
+        secure: true,
       },
     },
   },
